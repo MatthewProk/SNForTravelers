b2dbbfa83162e6499876befc0b25a5107031e64a
matthew <oneloveonemotiv@gmail.com>
2017-04-01 22:22:16 +0300

    added two methods in MessageDAOHibernate

--- src/main/java/DAO/hibernateDAO/MessageDAOHibernate.java
+++ src/main/java/DAO/hibernateDAO/MessageDAOHibernate.java
@@ -6,10 +6,13 @@ import model.Message;
 import model.User;
 import org.hibernate.Session;
 import org.hibernate.query.Query;
+import org.jboss.logging.Logger;
 
 import java.util.List;
 
 public class MessageDAOHibernate implements MessageDAO {
+
+    public static final Logger logger = Logger.getLogger(FriendDAOHibernate.class);
 
     @Override
     public void sendMessage(Message message) {
@@ -21,7 +24,7 @@ public class MessageDAOHibernate implements MessageDAO {
             session.save(message);
             session.getTransaction().commit();
         } catch (Exception e) {
-//        logger.error("Can't save message " + e);
+            logger.error("Can't save message " + e);
         } finally {
             if (session != null && session.isOpen()) {
                 session.close();
@@ -46,7 +49,7 @@ public class MessageDAOHibernate implements MessageDAO {
             messagesList = query.getResultList();
 
         } catch (Exception e) {
-//            logger.error("Can't get list of messages " + e);
+            logger.error("Can't get list of messages " + e);
         } finally {
             if (session != null && session.isOpen()) {
                 session.close();
@@ -54,4 +57,73 @@ public class MessageDAOHibernate implements MessageDAO {
         }
         return messagesList;
     }
+
+    public void deleteMessage(String textMessage) {
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+
+            Query query =  session.createQuery("delete  from Message " +
+                    "where textMessage ='" + textMessage + "'");
+            query.executeUpdate();
+
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            logger.error("Can't delete Friendship " + e);
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+//            HibernateUtil.shutdown();
+        }
+    }
+
+    public List<Message> getAListOfSentMessages(User sender) {
+        Session session = null;
+        List<Message> messagesList = null;
+        String senderLogin = sender.getLogin();
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            Query query =
+                    session.createQuery("from Message " +
+                            "where sender.login='" + senderLogin + "'");
+            session.getTransaction().commit();
+            messagesList = query.getResultList();
+
+        } catch (Exception e) {
+            logger.error("Can't get list of messages " + e);
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
+        return messagesList;
+    }
+
+    public List<Message> getAListOfReceivedMessages(User receiver) {
+        Session session = null;
+        List<Message> messagesList = null;
+        String receiverLogin = receiver.getLogin();
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            Query query =
+                    session.createQuery("from Message " +
+                            "where receiver.login='" + receiverLogin + "'");
+            session.getTransaction().commit();
+            messagesList = query.getResultList();
+
+        } catch (Exception e) {
+            logger.error("Can't get list of messages " + e);
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
+        return messagesList;
+    }
+
+
 }
