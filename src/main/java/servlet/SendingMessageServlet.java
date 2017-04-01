783e8f2444e2c0aa1d30ddd3f7940e96b4274bed
matthew <oneloveonemotiv@gmail.com>
2017-04-01 11:48:10 +0300

    added SendingMessageServlet and the servlet logic is done

---
+++ src/main/java/servlet/SendingMessageServlet.java
@@ -0,0 +1,36 @@
+package servlet;
+
+import DAO.hibernateDAO.MessageDAOHibernate;
+import model.Message;
+import model.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+
+@WebServlet(name = "SendingMessageServlet")
+public class SendingMessageServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        MessageDAOHibernate messageDAO = new MessageDAOHibernate();
+        String textMessage = request.getParameter("textMessage");
+
+        User receiver = (User) request.getSession().getAttribute("receiver");
+        User sender = (User) request.getSession().getAttribute("user");
+
+        Message message = new Message(sender, receiver, textMessage,new Date());
+        messageDAO.sendMessage(message);
+
+        request.setAttribute("messagesList", messageDAO.getMessageList(sender,receiver));
+        request.getRequestDispatcher("dialog.jsp").forward(request,response);
+//        logger.info("Message successful send");
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
