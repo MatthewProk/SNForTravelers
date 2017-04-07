2bc33439faa2b7e009e670355e236133c79a9d61
matthew <oneloveonemotiv@gmail.com>
2017-04-07 16:40:14 +0300

    changed servlet

--- src/main/java/servlet/AllUsersServlet.java
+++ src/main/java/servlet/AllUsersServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @WebServlet(name = "AllUsersServlet")
@@ -16,7 +17,14 @@ public class AllUsersServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         List<User> usersList = new UserDAOHibernate().getAllUsers();
-        request.getSession().setAttribute("usersList",usersList);
+        List<User> friendsList = (List<User>) request.getSession().getAttribute("friendsList");
+        List<User> otherUsers = new ArrayList<>();
+        for(User u:usersList){
+            if(!friendsList.contains(u)){
+                otherUsers.add(u);
+            }
+        }
+        request.getSession().setAttribute("lusersList",otherUsers);
         request.getRequestDispatcher("/allUsers.jsp").forward(request,response);
     }
 
